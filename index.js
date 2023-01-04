bebc498e2f5194239cb58c55e595502671f2a341
Will Tuffin <tuff2dev@gmail.com>
2023-01-04 08:31:34 +0000

    Add public frontend

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log("123");
+const path = require("path");
 
 const express = require("express");
 const dotenv = require("dotenv").config();
@@ -10,6 +10,9 @@ const app = express();
 // Enable body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/openai", require("./routes/openaiRoutes"));
 
